63e3b192c89b23f80e1378416b45622dbb02ba45
DQ <1515614009@.qq.com>
2018-08-13 17:04:20 +0800

    分支上的第二次提交
    创建控制器文件,引入路由功能

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,20 @@
-//创建app应用模块
-var yike=angular.module("yike",[]);
+//创建app应用模块,依赖模块:控制器模块ctrls,路由模块ngRoute
+var yike=angular.module("yike",["ctrls","ngRoute"]);
 //调用run方法,该方法的作用是当模块创建好之后就可以直接执行
 //该模块依赖的是根作用域,子作用域通常是与控制器绑定的的,而这里要与应用绑定
-yike.run(["$rootScope",function($rootscope){
+yike.run(["$rootScope",function($rootScope){
     //给头部的a标签绑定toggle方法
     //点击a标签,整个面板向右滑动,或者向左滑动
-    $rootscope.left=false;//设置移动,初始值为fasle,表示不移动
-    $rootscope.toggle=function(){
+    $rootScope.left=false;//初始值为fasle,表示不移动
+    $rootScope.toggle=function(){
         //取反
-        $rootscope.left=!$rootscope.left;
+        $rootScope.left=!$rootScope.left;
         //对导航栏中所有的dd的移动进行设置
         //获取所有的dd
         var dd=document.querySelectorAll("dd");
         //遍历dd,将每一个dd设置位移
-        //当$rootscope.left的值为true时,需要向右移动,false时向左移动
-        if($rootscope.left){//导航栏向右显示(位移量0)
+        //当$rootScope.left的值为true时,需要向右移动,false时向左移动
+        if($rootScope.left){//$rootScope.left为true即点击a标签时,导航栏向右显示(位移量0)
             for(var i=0; i<dd.length; i++) {
                 dd[i].style.transitionDuration = (i + 1) * 0.15 + 's';
                 dd[i].style.transitionProperty = 'all';
@@ -33,3 +33,33 @@ yike.run(["$rootScope",function($rootscope){
         }
     }
 }]);
+
+//修复锚点值的改变
+yike.config(["$locationProvider",function($locationProvider){
+    $locationProvider.hashPrefix("");
+}]);
+
+//配置路由
+yike.config(["$routeProvider",function($routeProvider){
+    $routeProvider.when("/",{
+        redirectTo:"/index" //跳转到/index处理
+    }).when("/index",{
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"index" //调用index控制器
+    }).when("/older",{
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"older" //调用older控制器
+    }).when("/author",{
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"author" //调用author控制器
+    }).when("/category",{
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"category" //调用category控制器
+    }).when("/favourite",{ 
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"favourite" //调用favourite控制器
+    }).when("/settings",{
+        templateUrl:"./views/test.html", //将要在ng-view区域显示的视图(以index.html的路径为标准)
+        controller:"settings" //调用settings控制器
+    })
+}])
---
+++ scripts/cotrollers.js
@@ -0,0 +1,43 @@
+//该js主要用于定义控制器
+angular.module("ctrls",[])
+//创建导航栏的控制器,模拟导航栏数据,绑定传递过去
+.controller("navs",["$scope",function($scope){
+    $scope.navs=[
+        {link:"#/index",icon:"icon-home",text:"今日一刻"},
+        {link:"#/older",icon:"icon-file-empty",text:"往期内容"},
+        {link:"#/author",icon:"icon-pencil",text:"热门作者"},
+        {link:"#/category",icon:"icon-menu",text:"栏目浏览"},
+        {link:"#/favourite",icon:"icon-heart",text:"我的喜欢"},
+        {link:"#/settings",icon:"icon-cog",text:"设置"}
+    ]
+}])
+//创建index控制器
+.controller("index",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取index的数据";
+    $rootScope.num=0;
+}])
+//创建older控制器
+.controller("older",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取older的数据";
+    $rootScope.num=1;
+}])
+//创建author控制器
+.controller("author",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取author的数据";
+    $rootScope.num=2;
+}])
+//创建category控制器
+.controller("category",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取category的数据";
+    $rootScope.num=3;
+}])
+//创建favourite控制器
+.controller("favourite",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取favourite的数据";
+    $rootScope.num=4;
+}])
+//创建settings控制器
+.controller("settings",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.msg="控制器获取settings的数据";
+    $rootScope.mun=5;
+}])
